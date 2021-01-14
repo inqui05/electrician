95c28bcb838e79421340808010f20814cdaba2d2
Artsiom Tratsiuk <inqui05@gmail.com>
2021-01-14 20:45:18 +0300

    my-config of webpack

---
+++ src/index.js
@@ -0,0 +1,19 @@
+'use strict';
+import './index.html';
+import './css/animate.css';
+import './css/bootstrap.css';
+import './css/font-awesome.css';
+import './css/jquery.fancybox.css';
+import './css/lightgallery.min.css';
+import './css/settings.css';
+import './css/style.css';
+
+//import slide1 from './images/slide1.jpg';
+//import slide2 from './images/slide2.jpg';
+//import slide3 from './images/slide3.jpg';
+
+const ale = 'world!';
+const print = () => {
+    alert(`Hello ${ale}`);
+};
+print();
