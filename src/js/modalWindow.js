c53112a844e3933846c7271324cf1ba5189c9f35
Artsiom Tratsiuk <inqui05@gmail.com>
2021-01-15 00:15:21 +0300

    realized a menu navigation

--- src/js/modalWindow.js
+++ src/js/modalWindow.js
@@ -3,12 +3,17 @@ const modalWindow = () => {
     const modalClose = document.querySelector('.modal-close'),
         header = document.querySelector('.header.relative'),
         modalCallback = document.querySelector('.modal-callback'),
+        price = document.querySelector('#services'),
+        faq = document.querySelector('#faq'),
+        contacts = document.querySelector('#contacts'),
         modalOverlay = document.querySelector('.modal-overlay');
     
     const showMenu = () => {
         modalCallback.style.display = 'block';
         modalOverlay.style.display = 'block';
     };
+
+    //console.log(price);
 
     const closeMenu = () => {
         modalCallback.style.display = 'none';
@@ -21,10 +26,22 @@ const modalWindow = () => {
         if (target.tagName === 'A' && target.classList.contains('callback-btn')){
             event.preventDefault();
             showMenu();
-        }
-    
-        if(target.classList.contains('mob-menu-btn') || (target.tagName === 'SPAN' && target.closest('.mob-menu-btn'))){
+            return;
+        } else if(target.classList.contains('mob-menu-btn') || (target.tagName === 'SPAN' && target.closest('.mob-menu-btn'))){
             showMenu();
+            return;
+        } else if (target.tagName === 'A' && target.textContent === 'Цены на услуги') {
+            event.preventDefault();
+            price.scrollIntoView({behavior: "smooth"});
+            return;
+        } else if (target.tagName === 'A' && target.textContent === 'Ответы на вопросы') {
+            event.preventDefault();
+            faq.scrollIntoView({behavior: "smooth"});
+            return;
+        } else if (target.tagName === 'A' && target.textContent === 'Контакты') {
+            event.preventDefault();
+            contacts.scrollIntoView({behavior: "smooth"});
+            return;
         }
     });
 
