cb6cc907651b7133710acd9162912e39005f6a2e
Artsiom Tratsiuk <inqui05@gmail.com>
2021-01-16 18:46:12 +0300

    add button Up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 'use strict';
 import modalWindow from './js/modalWindow';
 import slider from './js/slider';
+import buttonUp from './js/buttonUp';
 
 modalWindow();
 slider();
+buttonUp();
---
+++ src/js/buttonUp.js
@@ -0,0 +1,22 @@
+'use strict';
+const buttonUp = () => {
+    const up = document.querySelector('.up'),
+        servicesSection = document.querySelector('.title-h2'),
+        top = document.querySelector('.main-wrapper');
+
+    up.style.display = 'none';
+
+    window.addEventListener('scroll', () => {
+        if (servicesSection.getBoundingClientRect().top > 125) {
+            up.style.display = 'none';
+        } else if (servicesSection.getBoundingClientRect().top <= 125) {
+            up.style.display = 'block';
+        }
+    });
+
+    up.addEventListener('click', () => {
+        console.log(top);
+        top.scrollIntoView({behavior: "smooth"});
+    });
+};
+export default buttonUp;
