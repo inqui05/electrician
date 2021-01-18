2a06f622f80401c47b67db8acccf3bec29460f15
Artsiom Tratsiuk <inqui05@gmail.com>
2021-01-18 12:03:59 +0300

    add sending the form to the server

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import slider from './js/slider';
 import buttonUp from './js/buttonUp';
 import carousel from './js/carousel';
 import faq from './js/faq';
+import sendForm from './js/sendForm';
 
 modalWindow();
 slider();
 buttonUp();
 carousel();
 faq();
+sendForm();
