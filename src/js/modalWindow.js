71f594ff4262f788e262cc830f963aa218ab402c
Artsiom Tratsiuk <inqui05@gmail.com>
2021-01-19 13:31:47 +0300

    added mobile menu

--- src/js/modalWindow.js
+++ src/js/modalWindow.js
@@ -1,68 +1,75 @@
 'use strict';
 const modalWindow = () => {
-    const modalClose = document.querySelector('.modal-close'),
-        servicesElements = document.querySelector('.services-elements'),
-        header = document.querySelector('.header.relative'),
-        modalCallback = document.querySelector('.modal-callback'),
+    const modalCallback = document.querySelector('.modal-callback'),
         price = document.querySelector('#services'),
         faq = document.querySelector('#faq'),
         contacts = document.querySelector('#contacts'),
         modalOverlay = document.querySelector('.modal-overlay'),
-        buttonServices = document.querySelector('.button-services');
+        mobileMenu = document.querySelector('.mobile-menu');
     
-    const showMenu = () => {
+    const showCallback = () => {
         modalCallback.style.display = 'block';
         modalOverlay.style.display = 'block';
     };
 
-    const closeMenu = () => {
+    const closeCallback = () => {
         modalCallback.style.display = 'none';
         modalOverlay.style.display = 'none';
     };
+
+    const showMenu = () => {
+        mobileMenu.classList.add('open');
+    };
+
+    const closeMenu = () => {
+        mobileMenu.classList.remove('open');
+    };
     
-    header.addEventListener('click', event => {
+    window.addEventListener('click', event => {
         const target = event.target;
     
         if (target.tagName === 'A' && target.classList.contains('callback-btn')){
             event.preventDefault();
-            showMenu();
+            if (mobileMenu.classList.contains('open')) {
+                closeMenu();
+            }
+            showCallback();
             return;
+        } else if(target.classList.contains('mobile-menu-close') || target.classList.contains('overlay')) {
+            closeMenu();
         } else if(target.classList.contains('mob-menu-btn') || (target.tagName === 'SPAN' && target.closest('.mob-menu-btn'))){
             showMenu();
             return;
         } else if (target.tagName === 'A' && target.textContent === 'Цены на услуги') {
             event.preventDefault();
+            if (mobileMenu.classList.contains('open')) {
+                closeMenu();
+            }
             price.scrollIntoView({behavior: "smooth"});
             return;
         } else if (target.tagName === 'A' && target.textContent === 'Ответы на вопросы') {
             event.preventDefault();
+            if (mobileMenu.classList.contains('open')) {
+                closeMenu();
+            }
             faq.scrollIntoView({behavior: "smooth"});
             return;
         } else if (target.tagName === 'A' && target.textContent === 'Контакты') {
             event.preventDefault();
+            if (mobileMenu.classList.contains('open')) {
+                closeMenu();
+            }
             contacts.scrollIntoView({behavior: "smooth"});
             return;
-        }
-    });
-
-    buttonServices.addEventListener('click', () => {
-        showMenu();
-    });
-
-    modalOverlay.addEventListener('click', () => {
-        closeMenu();
-    });
-
-    modalClose.addEventListener('click', () => {
-        closeMenu();
-    });
-
-    servicesElements.addEventListener('click', event => {
-        const target = event.target;
-
-        if (target.tagName === 'A') {
+        } else if (target.classList.contains('button-services')) {
+            showCallback();
+        } else if (target.classList.contains('modal-overlay')) {
+            closeCallback();
+        } else if (target.tagName === 'IMG' && target.closest('.modal-close')) {
+            closeCallback();
+        } else if (target.tagName === 'A' && target.classList.contains('absolute')) {
             event.preventDefault();
-            showMenu();
+            showCallback();
         }
     });
 };
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,6 +7,9 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './dist'),
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
     mode: 'production',
     module: {
         rules: [
